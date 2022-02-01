36c73ffe532e16187486e3d6ad1549b920d5d843
Will Davis <willdavis576@gmail.com>
2022-02-01 11:02:57 +0000

    returns joke

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
+import requests
 
+url = 'https://icanhazdadjoke.com/'
+headers = {"Accept" : "application/json"}
+r = requests.get(url, headers=headers)
+r = r.json()
+
+joke = r["joke"]
+
+print(joke)
